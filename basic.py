637d18c71df4ed2320e242641e572feb87113663
Nikhil Yadav <nickrao93111@gmail.com>
2026-03-27 22:13:46 +0530

    Added numerical operations

--- basic.py
+++ basic.py
@@ -1,1 +1,7 @@
-print("Hello Numerical Python")
+import numpy as np
+
+numbers = np.array([1, 2, 3, 4])
+
+print("Original:", numbers)
+print("Square:", numbers ** 2)
+print("Sum:", np.sum(numbers))
